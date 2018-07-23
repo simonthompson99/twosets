cb9fe949a3c79e803274d9c903aaa00c71efb221
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2018-07-23 17:10:14 +0100

    removed numpy dependenct

---
+++ README.md
@@ -0,0 +1,3 @@
+# twosets
+
+twosets is a small python application that easily compares two lists of data and outputs a table showing which element was in which list and which was in both lists. 
--- twosets.py
+++ twosets.py
@@ -1,61 +1,81 @@
 from xerox import paste,copy
-from numpy import unique,in1d
 from re import split
 
+# get data from clipboard
 def getClipboardData():
-    #p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
-    #retcode = p.wait()
-    #data = p.stdout.read()
-    #return data.decode()
     return paste()
 
+# send data to clipboard
 def setClipboardData(data):
-    #p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
-    #p.stdin.write(data)
-    #p.stdin.close()
-    #retcode = p.wait()
     copy(data)
 
+# make intro to program
 def hello():
     print("-" * 61)
     print("#-" * 13 + " TWOSETS " + "#-" * 13)
     print("-" * 61)
 
+# split the data which comes in as a long string with linebreaks into separate list elements
+# removes quotes and tabs from either side of string too
 def splitData(string, delim):
     l = split(delim, string)
-    return list(filter(None, [o.strip(' \r\n\t"') for o in l]))
+    return list(filter(None, [o.strip(' \r\n\t"\'') for o in l]))
 
+# prints a little preview of a list
 def prListHead(data, nrows):
     print('\n'.join(data[0:nrows]))
     print('...')
 
+# gets name and data for a set, splits it, prints top of it, then returns a sorted, unique list of the elements
 def getSet():
     n = input("Copy set to the clipboard and tell me the name: ")
     d = getClipboardData()
     l = splitData(d,'\r|\n')
     print(n + " looks like:")
     prListHead(l, 5)
-    return([n,l])
+    return([n,sorted(list(set(l)))])
 
+# compares the one of the sets aaginst the meta-set
+# since they're sorted we can just step through saying what matches what
+# if we find a match in the set then mvoe forward in the set, if not, stay where we were in the list and move 
+# forward in the meta-set
+def compareTwoSortedLists(all, s1):
+    out = []
+    s1index = 0
+    for allindex in range(0, len(all)):
+        if all[allindex] == s1[min(s1index, len(s1) - 1)]:
+            out.append(True)
+            s1index+=1
+        else:
+            out.append(False)
+    return out
+
+# makes boolean list to a string list
+def toStr(lst):
+    return [str(i) for i in list(lst)]
+
+# compares two lists and returns some summary stats
+# makes single unique list from both lists
+# compares each list against meta-list
+# returns boolean lists for what is in what
 def setCompare(s1, s2):
-    all = unique(s1[1] + s2[1])
-    inone = in1d(all, s1[1])
-    intwo = in1d(all, s2[1])
-    inboth = inone & intwo
+    all = sorted(list(set(s1[1] + s2[1])))
+    inone = compareTwoSortedLists(all, s1[1])
+    intwo = compareTwoSortedLists(all, s2[1])
+    inboth = [a and b  for a, b in  zip(inone, intwo)]
     print("Number of unique ids: " + str(len(all)))
     print("Number missing from " + s1[0] + ": " + str(len(all) - sum(inone)))
     print("Number missing from " + s2[0] + ": " + str(len(all) - sum(intwo)))
     print("Number in both sets: " + str(sum(inboth)))
-    return [list(all), arrToStr(inone), arrToStr(intwo), arrToStr(inboth)]
+    return [all, toStr(inone), toStr(intwo), toStr(inboth)]
 
+# writes out the table from setCompare to clipboard
 def exportArray(arr):
     l = zip(arr[0], arr[1], arr[2], arr[3])
     s = 'id\tin '+one[0]+'\tin '+two[0]+'\tin both\n'+'\n'.join(['\t'.join(i) for i in l])
     setClipboardData(s)
     print("Table written to clipboard")
 
-def arrToStr(arr):
-    return [str(i) for i in list(arr)]
 
 while True:
     hello()
@@ -63,19 +83,3 @@ while True:
     two = getSet()
     final = setCompare(one, two)
     exportArray(final)
-
-#inone = [i in one[1] for i in all]
-#intwo = [i in two[1] for i in all]
-#inboth = [i in one[1].intersection(two[1]) for i in all] 
-#
-#inonestr = [str(i) for i in inone]
-#intwostr = [str(i) for i in intwo]
-#inbothstr = [str(i) for i in inboth]
-#
-
-#out = [header] + list(zip(all, inonestr, intwostr, inbothstr))
-#celljoiner = "\t".join
-#rowjoiner = "\r".join
-#exp = rowjoiner([celljoiner(cells) for cells in out])
-#
-#setClipboardData(exp.encode())
